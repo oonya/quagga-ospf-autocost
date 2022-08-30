f94ff67633f52e90a78afc3e312c196d734eec95
oonya <oonya200216@gmail.com>
2022-08-30 18:02:10 +0900

    feat: 2つのaddressに対してping, コストセットする関数を呼び出す

--- main.go
+++ main.go
@@ -5,16 +5,61 @@ import (
 	"github.com/go-ping/ping"
 )
 
+// TODO: Peerよりいい名前募
+type Peer struct {
+	remtoeAddress string
+	remoteIf string
+	localAddress string
+	localIf string
+}
+
 func main() {
-	pinger, err := ping.NewPinger("www.google.com")
-	pinger.SetPrivileged(true)
+	nic1 := Peer{remtoeAddress: "192.168.130.3", remoteIf: "enp0s10", localIf: "enp0s10", localAddress: "localhost"}
+	nic2 := Peer{remtoeAddress: "10.10.10.3", remoteIf: "srsgre", localIf: "srsgre", localAddress: "localhost"}
+
+	localPinger, err := ping.NewPinger(nic1.remtoeAddress)
+	localPinger.SetPrivileged(true)
+	localPinger.Count = 1
 	if err != nil {
 			panic(err)
 	}
-	pinger.Count = 1
-	pinger.Run()
-	stats := pinger.Statistics()
-	fmt.Printf("loss: %f\n", stats.PacketLoss)
-	fmt.Printf("Rtts: %s\n", stats.Rtts)
-	fmt.Printf("AvgRtt: %s\n", stats.AvgRtt)
+
+	remotePinger, err := ping.NewPinger(nic2.remtoeAddress)
+	remotePinger.SetPrivileged(true)
+	remotePinger.Count = 1
+	if err != nil {
+			panic(err)
+	}
+
+	// TODO: 無限ループに
+	for i := 0; i < 3; i++ {
+		localPinger.Run()
+		remotePinger.Run()
+	
+		// TODO: 並列化
+		localStats := localPinger.Statistics()
+		fmt.Printf("loss1: %f\n", localStats.PacketLoss)
+		fmt.Printf("AvgRtt1: %s\n", localStats.AvgRtt)
+
+		remoteStats := remotePinger.Statistics()
+		fmt.Printf("loss2: %f\n", remoteStats.PacketLoss)
+		fmt.Printf("AvgRtt2: %s\n", remoteStats.AvgRtt)
+
+		if localStats.AvgRtt < remoteStats.AvgRtt {
+			if err := setCost(1, nic1.localAddress, nic1.localIf); err != nil {
+				panic(err)
+			}
+			if err := setCost(2, nic1.remtoeAddress, nic1.remoteIf); err != nil {
+				panic(err)
+			}
+		}
+
+		fmt.Println()
+		// TODO: loging & sleep
+	}
+}
+
+func setCost(cost int, addr string, nic string) error {
+	// TODO: exec shell script
+	return nil
 }
