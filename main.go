5104768d02e11727ed8fd7e5e05b073bc364149e
oonya <oonya200216@gmail.com>
2022-09-01 18:09:42 +0900

    feat: 優先パスが変更されたときのみsetCostを呼ぶようにした

--- main.go
+++ main.go
@@ -2,31 +2,32 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"os"
 	"github.com/go-ping/ping"
 	"log"
+	"os"
+	"time"
 )
 
 // TODO: Peerよりいい名前募
 type Peer struct {
 	remtoeAddress string
-	remoteIf string
-	localAddress string
-	localIf string
+	remoteIf      string
+	localAddress  string
+	localIf       string
+	preffered     bool
 }
 
 func main() {
-	wlanNic := Peer{remtoeAddress: "192.168.130.1", remoteIf: "enp0s10", localIf: "enp0s10", localAddress: "localhost"}
-	ranNic := Peer{remtoeAddress: "10.10.10.1", remoteIf: "srsgre", localIf: "srsgre", localAddress: "localhost"}
+	wlanNic := Peer{remtoeAddress: "192.168.130.1", remoteIf: "enp0s10", localIf: "enp0s10", localAddress: "localhost", preffered: false}
+	ranNic := Peer{remtoeAddress: "10.10.10.1", remoteIf: "srsgre", localIf: "srsgre", localAddress: "localhost", preffered: true}
 
 	file, err := os.OpenFile("zero.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-    if err != nil {
-        panic(err)
-    }
-    defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 	log.SetOutput(file)
-	
+
 	// TODO: 無限ループに
 	for i := 0; i < 3; i++ {
 		wlanPinger, err := ping.NewPinger(wlanNic.remtoeAddress)
@@ -35,18 +36,18 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		
+
 		ranPinger, err := ping.NewPinger(ranNic.remtoeAddress)
 		ranPinger.SetPrivileged(true)
 		ranPinger.Count = 1
 		if err != nil {
-				panic(err)
+			panic(err)
 		}
 
 		// TODO: 並列化
 		wlanPinger.Run()
 		ranPinger.Run()
-	
+
 		wlanStats := wlanPinger.Statistics()
 		fmt.Printf("loss1: %f\n", wlanStats.PacketLoss)
 		fmt.Printf("AvgRtt1: %s\n", wlanStats.AvgRtt)
@@ -55,14 +56,27 @@ func main() {
 		fmt.Printf("loss2: %f\n", ranStats.PacketLoss)
 		fmt.Printf("AvgRtt2: %s\n", ranStats.AvgRtt)
 
-		if wlanStats.AvgRtt < ranStats.AvgRtt {
-			if err := setCost(1, wlanNic.localAddress, wlanNic.localIf); err != nil {
-				panic(err)
+		if wlanStats.AvgRtt < ranStats.AvgRtt && !wlanNic.preffered {
+			if err = setCost(1, &wlanNic); err != nil {
+				log.Panicf("cost of wlan couldnt set to %d\n", 1)
 			}
-			if err := setCost(2, wlanNic.remtoeAddress, wlanNic.remoteIf); err != nil {
-				panic(err)
+			if err = setCost(2, &ranNic); err != nil {
+				log.Panicf("cost of ran couldnt set to %d\n", 2)
 			}
-			// TODO: nic2に対してもsetCostを呼ぶ
+			wlanNic.preffered = true
+			ranNic.preffered = false
+			log.Println("wlan became priority")
+		}
+		if ranStats.AvgRtt < wlanStats.AvgRtt && !ranNic.preffered {
+			if err = setCost(1, &ranNic); err != nil {
+				log.Panicf("cost of ran couldnt set to %d\n", 1)
+			}
+			if err = setCost(2, &wlanNic); err != nil {
+				log.Panicf("cost of wlan couldnt set to %d\n", 2)
+			}
+			ranNic.preffered = true
+			wlanNic.preffered = false
+			log.Println("RAN became priority")
 		}
 
 		fmt.Println()
@@ -70,8 +84,9 @@ func main() {
 	}
 }
 
-func setCost(cost int, addr string, nic string) error {
+func setCost(cost int, peer *Peer) error {
 	// TODO: exec shell script
-	log.Printf("cost of %s is set to %d", addr, cost)
+	log.Printf("cost of %s is set to %d", peer.localAddress, cost)
+	log.Printf("cost of %s is set to %d", peer.remtoeAddress, cost)
 	return nil
 }
